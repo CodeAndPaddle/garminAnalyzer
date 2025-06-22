cc1945bd7d895c97956fcde9649adb31df553cdf
Ron Benjano <benjano9397@gmail.com>
2025-06-22 12:47:53 +0200

    a python script that fetch a workout data from Garmin and does it own analysis: When you paddle its not convenient to press the lap button every time you start and stop and interval. so the script detect automatically when you start (accelerate) and when you stop (decelerate) and gather all the intervals together. fun side project

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import datetime
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'hHi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 def fetch_activity_from_garmin():
     # 1) Your Garmin login
